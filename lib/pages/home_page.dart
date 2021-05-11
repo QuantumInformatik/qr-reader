049a7d73b245b025cfa1d7b3182334d9623442b5
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-05-10 19:42:17 -0500

    149. diseño de la app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/widgets/ScanButton.dart';
+import 'package:qr_reader/widgets/custom_navigationbar.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Historial'),
+        actions: [
+          IconButton(icon: Icon(Icons.delete_forever), onPressed: (){})
+        ],
+      ),
       body: Center(
         child: Text('Home page'),
       ),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
